24bad0fd77cd61b585804682b9a7b07543c3b656
flyinacres <ron-github@rfgf.com>
2025-03-15 10:00:00 -0700

    Basically working at this time

--- extract_stats.py
+++ extract_stats.py
@@ -13,7 +13,7 @@ def setup_logging():
     os.makedirs("logs", exist_ok=True)
     logging.basicConfig(
         filename="logs/extract_stats.log",
-        level=logging.DEBUG,
+        level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(message)s",
     )
 
